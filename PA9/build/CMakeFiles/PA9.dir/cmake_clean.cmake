0ddc9a82bbcb909ea14d5e8a180f66ad1efb0d84
jdj1121 <djia@nevada.unr.edu>
2018-11-07 00:41:41 +0000

    fixed spotlight

---
+++ PA9/build/CMakeFiles/PA9.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PA9.dir/src/camera.cpp.o"
+  "CMakeFiles/PA9.dir/src/engine.cpp.o"
+  "CMakeFiles/PA9.dir/src/graphics.cpp.o"
+  "CMakeFiles/PA9.dir/src/main.cpp.o"
+  "CMakeFiles/PA9.dir/src/mesh.cpp.o"
+  "CMakeFiles/PA9.dir/src/object.cpp.o"
+  "CMakeFiles/PA9.dir/src/shader.cpp.o"
+  "CMakeFiles/PA9.dir/src/window.cpp.o"
+  "PA9.pdb"
+  "PA9"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PA9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

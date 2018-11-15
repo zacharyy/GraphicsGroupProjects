4d2b37e076463403028dc8da0e7a82a837143549
jdj1121 <djia@nevada.unr.edu>
2018-11-15 21:21:32 +0000

    new models and textures

--- PA10/build/CMakeFiles/PA10_SUCCESSFUL.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PA10_SUCCESSFUL"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PA10_SUCCESSFUL.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()

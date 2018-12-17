6f4d746ecef024737f41fdf03531365886abb439
Ethan M Park <ethanpark@gnx1.rd.unr.edu>
2018-12-17 05:39:47 -0800

    removed build folder

--- PA11/build/CMakeFiles/PA11_SUCCESSFUL.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PA11_SUCCESSFUL"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PA11_SUCCESSFUL.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
